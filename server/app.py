099fa311c27b5917a77f9ecc0e0266bbc61b58ab
Kennedy Muinde <kennedymuinde@gmail.com>
2023-06-23 09:06:15 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -16,12 +16,43 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [n.to_dict() for n in Plant.query.all()]
+
+        response = make_response(jsonify(plants), 200)
+
+        return response
+
+    def post(self):
+        new_plant = Plant()
+
+        for key, value in request.json.items():
+            setattr(new_plant, key, value)
+            
+        # or new_plant = Plant(**request.get_json())
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(jsonify(new_plant.to_dict()), 201)
+
+        return response
+
+
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(jsonify(plant), 200)
+        return response
+
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
